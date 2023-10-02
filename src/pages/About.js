143616cdd8cd307c4ef65df83d9fbaeba309e5f7
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-10-02 15:07:32 +0200

    TP terminé

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo.js';
 
 const About = () => {
 	return (
 		<div>
+			<Logo />
+			<Navigation />
 			<h1>A propos</h1>
 			<br />
 			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat dolorem sit quae inventore atque dignissimos tenetur, corporis officia ex alias, sunt consectetur delectus, veniam architecto excepturi aliquam. Nesciunt ipsa aliquid modi expedita quasi beatae? Nesciunt iure voluptate officiis explicabo tenetur, porro sunt vitae maxime amet repellendus! Laborum quo earum adipisci dolorum molestias quod error velit, unde beatae facilis praesentium fuga consequatur fugiat doloremque veniam repellat ullam voluptatum accusantium atque rerum ipsum suscipit eum aliquid mollitia. Sunt suscipit consectetur vel nihil quaerat veritatis nam odio enim placeat unde ad, alias expedita nobis. Veniam, iste? Consequatur, ullam. Cumque assumenda minima fugiat inventore.</p>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Logo from '../components/Logo.js';
+import Navigation from '../components/Navigation';
+import Countries from '../components/Countries';
 
 const Home = () => {
 	return (
 		<div>
-			
+			<Logo />
+			<Navigation />
+			<Countries />
 		</div>
 	);
 };
