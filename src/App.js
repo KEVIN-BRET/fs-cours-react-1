34be4642bcad45de3b6f1ecdd5321ea3cdf727fd
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-10-02 11:33:38 +0200

    Création des routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
-
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
 
 const App = () => {
   return (
-    <h1>Hello React</h1>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<div>
+			<h1>A propos</h1>
+			<br />
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat dolorem sit quae inventore atque dignissimos tenetur, corporis officia ex alias, sunt consectetur delectus, veniam architecto excepturi aliquam. Nesciunt ipsa aliquid modi expedita quasi beatae? Nesciunt iure voluptate officiis explicabo tenetur, porro sunt vitae maxime amet repellendus! Laborum quo earum adipisci dolorum molestias quod error velit, unde beatae facilis praesentium fuga consequatur fugiat doloremque veniam repellat ullam voluptatum accusantium atque rerum ipsum suscipit eum aliquid mollitia. Sunt suscipit consectetur vel nihil quaerat veritatis nam odio enim placeat unde ad, alias expedita nobis. Veniam, iste? Consequatur, ullam. Cumque assumenda minima fugiat inventore.</p>
+			<br />
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat dolorem sit quae inventore atque dignissimos tenetur, corporis officia ex alias, sunt consectetur delectus, veniam architecto excepturi aliquam. Nesciunt ipsa aliquid modi expedita quasi beatae? Nesciunt iure voluptate officiis explicabo tenetur, porro sunt vitae maxime amet repellendus! Laborum quo earum adipisci dolorum molestias quod error velit, unde beatae facilis praesentium fuga consequatur fugiat doloremque veniam repellat ullam voluptatum accusantium atque rerum ipsum suscipit eum aliquid mollitia. Sunt suscipit consectetur vel nihil quaerat veritatis nam odio enim placeat unde ad, alias expedita nobis. Veniam, iste? Consequatur, ullam. Cumque assumenda minima fugiat inventore.</p>
+		</div>
+	);
+};
+
+export default About;
